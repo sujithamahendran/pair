68d93942ca04c8bb07b5f0ce0a91fff885b935b5
sujithamahendran <sujirenu2@gmail.com>
2017-06-27 11:52:26 +0530

    Create lexi.c

---
+++ lexi.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a,b;
+printf("enter the string:");
+scanf("%c\n%c",&a,&b);
+if(a> 'a '||a<'z')
+{
+else if(a>b)
+{
+printf("%c is a largest string");
+}
+else
+{
+printf("%c it is not a string");
+}
+getch();
+}
